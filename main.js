23eb1a00de928e0a9033c9b487e2e7400c9183cd
kazuki <something@gmail.com>
2021-01-20 08:27:41 +0900

    implement reverse logic

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
-// change player
-let player = 1; // 1 is black
+//-----------------PREPARING------------------
+let player = 2; 
+let whiteStone = "○";
+let blackStone = "●";
+let currentStone = blackStone;
 
 // get dom elemtns of cell
-const cell = document.getElementsByClassName("cell");
+var cell = document.getElementsByClassName("cell");
 
-for(let i = cell.length - 1; i >= 0; i--){
-        cellAction(cell[i])
-　　}
+//-----------------MAIN PROCESS------------------
+for(let i = 0; i < cell.length - 1;  i++){
+    cell[i].addEventListener("click", function(){
+        // to Array
+        let cellElements = [].slice.call(cell);
+        let index = cellElements.indexOf(this);
 
-function cellAction(cellDOM){
-    // register to eventlistener 
-    cellDOM.addEventListener("click", function(){
-        this.classList.toggle("active");
-        playersAction();
-        this.classList.remove("active")
-    });
+        putStone(index);
+        changeStone(index);
+        changeOrder();
+        console.log(currentStone);
+        console.log(player);
+
+        headTextChange();
+    })};
+
+//-----------------FUNCTIONS------------------
+// put stone
+const putStone = (index) =>{
+    cell[index].innerHTML = currentStone;
 }
 
-// click and put stone
-const playersAction = () => {
-    const stone = (player % 2 ==0 ) ? "○" :  "●" // identify player
-    const elem = document.getElementsByClassName("active")[0]; 
-    elem.innerHTML = stone;
-
-    // turn change
-    const headText = (player % 2 ==0 ) ? "Black Turn" :  "White Turn"
-    changeTurn(headText);
-    player += 1;
+// change order
+const changeOrder = () =>{
+    if(player % 2 == 0){
+        currentStone = whiteStone;
+        player ++;
+    } else {
+        currentStone = blackStone;
+        player ++;
+    }
 }
 
-// chenge turn
-const changeTurn = (str) => {
+// change head text
+const headTextChange = () =>{
+    const headText = (player % 2 ==0) ? "Black Turn" :  "White Turn";
     const elem = document.getElementById("turnOfPlayer");
-    elem.innerHTML = str;
+    elem.innerHTML = headText;
+    return headText;
 }
 
 // jadge winner
@@ -46,19 +59,50 @@ const judgeWinner = () => {
     let countOfWhite = 0;
 
     for (i = 0; i < cellTexts.length; i++){
-        if(cellTexts[i] ==  "●") {countOfBlack ++;}
-        else if(cellTexts[i] == "○") {countOfWhite ++;}
+        if(cellTexts[i] ==  othelloBlack) {countOfBlack ++;}
+        else if(cellTexts[i] == othelloWhte) {countOfWhite ++;}
     }
-
-    console.log(countOfBlack);
-    console.log(countOfWhite);
 
     let judgingText = "";
     if(countOfBlack > countOfWhite) {judgingText = "BLACK IS WINNER!!!!"}
     else if(countOfBlack < countOfWhite) {judgingText = "WHITE IS WINNER!!!!"} 
 
-    console.log(judgingText);
-
     const elem = document.getElementById("turnOfPlayer");
     elem.innerHTML = judgingText;
+}
+
+// check whether there are different stones in top, right, left, and bottom sides
+const changeStone = (index) => {
+    let numIndex = 8;
+
+    // retrieve the color(value) of cell
+    let prevLeftCell = cell[index - 2].innerHTML;
+    let prevCell = cell[index - 1].innerHTML;
+    let nextRightCell = cell[index + 2].innerHTML;
+    let nextCell = cell[index + 1].innerHTML;
+
+    //// horizontal directions
+    /// left
+    // black
+    if (prevLeftCell.match(blackStone) && prevCell.match(whiteStone)){
+        let targetIndex = index - 1;
+        putStone(targetIndex);
+    }
+    // white
+    if (prevLeftCell.match(whiteStone) && prevCell.match(blackStone)){
+        let targetIndex = index - 1;
+        putStone(targetIndex);
+    }
+
+    /// right
+    // black
+    if (nextRightCell.match(blackStone) && nextCell.match(whiteStone)){
+        let targetIndex = index + 1;
+        putStone(targetIndex)
+    }
+    // white
+    if (nextRightCell.match(whiteStone) && nextCell.match(blackStone)){
+        let targetIndex = index + 1;
+        putStone(targetIndex)
+    }
 }
